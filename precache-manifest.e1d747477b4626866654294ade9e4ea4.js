ba3a51c84a0bed79e4efa65d7abc932b11b83912
Phạm Thanh Thường <phamthanhthuongbk@gmail.com>
2021-04-18 22:09:34 +0900

    deploy

---
+++ precache-manifest.e1d747477b4626866654294ade9e4ea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53db9ce2bd7223f17b13",
+    "url": "/vuejs-pwa/css/app.30a4e642.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuejs-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cd40e82db4c28ad4ce8ed4c95a1a8b3f",
+    "url": "/vuejs-pwa/index.html"
+  },
+  {
+    "revision": "53db9ce2bd7223f17b13",
+    "url": "/vuejs-pwa/js/app.dac634e5.js"
+  },
+  {
+    "revision": "8722d0577d8dcdd91cf9",
+    "url": "/vuejs-pwa/js/chunk-vendors.3e81f15e.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuejs-pwa/logo.png"
+  },
+  {
+    "revision": "2712911c919f25bf84cc1312c6aa2625",
+    "url": "/vuejs-pwa/manifest.json"
+  }
+]);
