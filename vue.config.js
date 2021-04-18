d9d46d952d83a486790b2b04c97030d0f823ce09
Phạm Thanh Thường <phamthanhthuongbk@gmail.com>
2021-04-18 22:04:48 +0900

    Update Icon PWA

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,31 @@
 module.exports = {
     pwa: {
-      name: 'My App',
+      manifestOptions: {
+        name: "Vue PWA",
+        short_name: "Vue PWA",
+        start_url: "./",
+        display: "standalone",
+        theme_color: "#42b983",
+        background_color: "#42b983",
+        icons: [
+          {
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
       themeColor: "#42b983",
       msTileColor: "#42b983",
       appleMobileWebAppCache: "yes",
-      manifestOptions: {
-        background_color: "#42b983"
-      },
       iconPaths: {
-        faviconSVG: 'src/assets/logo.png',
-        favicon32: 'src/assets/logo.png',
-        favicon16: 'src/assets/logo.png',
-        appleTouchIcon: 'src/assets/logo.png',
-        maskIcon: 'src/assets/logo.png',
-        msTileImage: 'src/assets/logo.png'
+        faviconSVG: 'logo.png',
+        favicon32: 'logo.png',
+        favicon16: 'logo.png',
+        appleTouchIcon: 'logo.png',
+        maskIcon: 'logo.png',
+        msTileImage: 'logo.png'
       }
     }
 };
