b3a9f8539b2160d92769ead8bcc97814788b806a
Phạm Thanh Thường <phamthanhthuongbk@gmail.com>
2021-04-18 22:14:57 +0900

    Update readme And add deploy.sh

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # vuejs-pwa
+- Vuejs3
+- Tailwind Css
+- PWA (cli-plugin-pwa)
+- Storybook (Bug currently)
+
+# Exam Page
+https://phamthanhthuongbk.github.io/vuejs-pwa/
 
 ## Project setup
 ```
@@ -22,3 +29,9 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### Deploy 
+```
+sh deploy.sh
+```
--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,32 @@
 module.exports = {
-    pwa: {
-      manifestOptions: {
-        name: "Vue PWA",
-        short_name: "Vue PWA",
-        start_url: "./",
-        display: "standalone",
-        theme_color: "#42b983",
-        background_color: "#42b983",
-        icons: [
-          {
-            src: "logo.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-      themeColor: "#42b983",
-      msTileColor: "#42b983",
-      appleMobileWebAppCache: "yes",
-      iconPaths: {
-        faviconSVG: 'logo.png',
-        favicon32: 'logo.png',
-        favicon16: 'logo.png',
-        appleTouchIcon: 'logo.png',
-        maskIcon: 'logo.png',
-        msTileImage: 'logo.png'
-      }
+  pwa: {
+    manifestOptions: {
+      name: "Vue PWA",
+      short_name: "Vue PWA",
+      start_url: "./",
+      display: "standalone",
+      theme_color: "#42b983",
+      background_color: "#42b983",
+      icons: [
+        {
+          src: "logo.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    iconPaths: {
+      faviconSVG: 'logo.png',
+      favicon32: 'logo.png',
+      favicon16: 'logo.png',
+      appleTouchIcon: 'logo.png',
+      maskIcon: 'logo.png',
+      msTileImage: 'logo.png'
     }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/vuejs-pwa/' : '/'
 };
